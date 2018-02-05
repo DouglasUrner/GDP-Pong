bf26d09425533767736fa15cb95d9616c894a6f9
Douglas Urner <dlu@canishe.com>
2018-02-05 00:04:41 -0800

    Add Rackets and their script.

---
+++ Pong/Assets/Scripts/Racket.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Racket : MonoBehaviour {
+	public float speed = 30;
+	public string axis = "Vertical";
+	
+	// Update is called once per frame
+	private void FixedUpdate () {
+		var v = Input.GetAxisRaw(axis);
+		GetComponent<Rigidbody2D>().velocity = new Vector2(0, v) * speed;
+	}
+}
